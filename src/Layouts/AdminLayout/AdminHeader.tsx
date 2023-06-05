5ef8907d289bc3257fbf36a3a1269afbdd37e753
msdmhmdi <masoudmhmdi86@gmail.com>
2023-06-05 18:12:04 +0430

    add berger menu in mobile adn tablet size

--- src/Layouts/AdminLayout/AdminHeader.tsx
+++ src/Layouts/AdminLayout/AdminHeader.tsx
@@ -3,6 +3,8 @@ import Toolbar from '@mui/material/Toolbar';
 import BasicTabs from '@/Components/Tabs';
 import React from 'react';
 import Image from 'next/image';
+import { Box, Button } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function AdminHeader() {
   return (
@@ -10,20 +12,50 @@ export default function AdminHeader() {
       <AppBar sx={{ backgroundColor: 'white' }}>
         <Toolbar
           sx={{
-            backgroundColor: 'white',
             display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
+            flexDirection: 'column',
+            justifyContent: 'center',
           }}
         >
-          <BasicTabs />
-          <Image
-            alt="lsdj"
-            width={179}
-            height={46}
-            style={{ width: '100%', maxWidth: '179px' }}
-            src={'/logo.png'}
-          />
+          <Box
+            sx={{
+              backgroundColor: 'white',
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <Box
+              sx={{
+                display: {
+                  sx: 'flex',
+                  md: 'none',
+                },
+              }}
+            >
+              <Button>
+                <MenuIcon fontSize="large" />
+              </Button>
+            </Box>
+            <Box
+              sx={{
+                display: {
+                  xs: 'none',
+                  md: 'block',
+                },
+              }}
+            >
+              <BasicTabs />
+            </Box>
+            <Image
+              alt="lsdj"
+              width={179}
+              height={46}
+              style={{ width: '100%', maxWidth: '179px' }}
+              src={'/logo.png'}
+            />
+          </Box>
         </Toolbar>
       </AppBar>
     </React.Fragment>
--- src/Layouts/MainLayout/MainHeader.tsx
+++ src/Layouts/MainLayout/MainHeader.tsx
@@ -6,6 +6,8 @@ import Slide from '@mui/material/Slide';
 import BasicTabs from '@/Components/Tabs';
 import React from 'react';
 import Image from 'next/image';
+import { Button } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 interface Props {
   /**
@@ -40,20 +42,50 @@ export default function MainHeader(props: Props) {
         <AppBar sx={{ backgroundColor: 'white' }}>
           <Toolbar
             sx={{
-              backgroundColor: 'white',
               display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
+              flexDirection: 'column',
+              justifyContent: 'center',
             }}
           >
-            <BasicTabs />
-            <Image
-              alt="lsdj"
-              width={179}
-              height={46}
-              style={{ width: '100%', maxWidth: '179px' }}
-              src={'/logo.png'}
-            />
+            <Box
+              sx={{
+                backgroundColor: 'white',
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+            >
+              <Box
+                sx={{
+                  display: {
+                    sx: 'flex',
+                    md: 'none',
+                  },
+                }}
+              >
+                <Button>
+                  <MenuIcon fontSize="large" />
+                </Button>
+              </Box>
+              <Box
+                sx={{
+                  display: {
+                    xs: 'none',
+                    md: 'block',
+                  },
+                }}
+              >
+                <BasicTabs />
+              </Box>
+              <Image
+                alt="lsdj"
+                width={179}
+                height={46}
+                style={{ width: '100%', maxWidth: '179px' }}
+                src={'/logo.png'}
+              />
+            </Box>
           </Toolbar>
         </AppBar>
       </HideOnScroll>
