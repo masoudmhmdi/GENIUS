319de12d6d86ab2aef4438e34e1fe9125160f879
msdmhmdi <masoudmhmdi86@gmail.com>
2023-06-15 09:35:50 +0430

    create a custom hook for delete product

--- src/api/services/useDeleteProduct/index.ts
+++ src/api/services/useDeleteProduct/index.ts
@@ -1,3 +1,15 @@
-function useDeleteProduct() {}
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import deleteProductService from './deleteProductService';
+
+function useDeleteProduct() {
+  return useMutation({
+    mutationFn: (id: string) => deleteProductService(id),
+    onSuccess: () => {
+      const queryClient = useQueryClient();
+      queryClient.invalidateQueries({ queryKey: ['GetProduct'] });
+    },
+  });
+}
 
 export default useDeleteProduct;
