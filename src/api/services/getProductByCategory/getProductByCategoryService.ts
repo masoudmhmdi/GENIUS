2c004e557536d3ee6b49eab203875f0a52a75dcc
msdmhmdi <masoudmhmdi86@gmail.com>
2023-06-20 16:37:29 +0430

    add a new service for get product by category

---
+++ src/api/services/getProductByCategory/getProductByCategoryService.ts
@@ -0,0 +1,11 @@
+import { serverReq } from '@/api/constants';
+
+async function getProductByCategoryService(id: string) {
+  const url = new URL('/products');
+  url.searchParams.append('category', id);
+
+  const res = await serverReq(url.toString());
+  return res.data;
+}
+
+export default getProductByCategoryService;
