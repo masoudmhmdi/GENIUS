b64f64dddab559236480b3adad9ebfa2986d888c
msdmhmdi <masoudmhmdi86@gmail.com>
2023-06-22 21:44:58 +0430

    develop single category page and sinc filter and sort option with state

---
+++ src/Components/FilterBar/index.tsx
@@ -0,0 +1,129 @@
+import * as React from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Box } from '@mui/material';
+
+type IFilterProps = {
+  setter: React.Dispatch<
+    React.SetStateAction<
+      | {}
+      | {
+          sort: string;
+          brand: string;
+        }
+    >
+  >;
+};
+
+export default function FilterBar({ setter }: IFilterProps) {
+  const handleRadioPriceChange = (value: string) => {
+    setter((prev) => {
+      return { ...prev, sort: value };
+    });
+  };
+  const handleRadioBrandChange = (value: string) => {
+    setter((prev) => {
+      return { ...prev, brand: value };
+    });
+  };
+  return (
+    <Box>
+      <Typography sx={{ padding: '8px' }} variant="h5" fontWeight={'bold'}>
+        فیلتر ها
+      </Typography>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>قیمت</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <FormControl>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+              onChange={(e) => handleRadioPriceChange(e.target.value)}
+            >
+              <FormControlLabel
+                value="-price"
+                control={<Radio />}
+                label="گران ترین"
+              />
+              <FormControlLabel
+                value="price"
+                control={<Radio />}
+                label="ارزان ترین"
+              />
+              <FormControlLabel value="" control={<Radio />} label="هیچ کدام" />
+            </RadioGroup>
+          </FormControl>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>برند</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            onChange={(e) => handleRadioBrandChange(e.target.value)}
+          >
+            <FormControlLabel value="apple" control={<Radio />} label="اپل" />
+            <FormControlLabel
+              value="sumsung"
+              control={<Radio />}
+              label="سامسونگ"
+            />
+            <FormControlLabel
+              value="xiaomi"
+              control={<Radio />}
+              label="شیائومی"
+            />
+            <FormControlLabel
+              value="xiaomi"
+              control={<Radio />}
+              label="شیائومی"
+            />
+            <FormControlLabel
+              value="huawei"
+              control={<Radio />}
+              label="هووای"
+            />
+          </RadioGroup>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Disabled Accordion</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
+}
--- src/pages/category/[id].tsx
+++ src/pages/category/[id].tsx
@@ -1,34 +1,54 @@
 import ProductCard from '@/Components/card';
-import RowRadioButtonsGroup from '@/Components/radioGroup';
+import RowRadioButtonsGroup from '@/Components/FilterBar';
 import useGetProductByCategory from '@/api/services/getProductByCategory';
 import getProductByCategoryService from '@/api/services/getProductByCategory/getProductByCategoryService';
+import { theme } from '@/theme';
 import { IProductFromBack } from '@/types';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { QueryClient, dehydrate } from '@tanstack/react-query';
 import { GetServerSideProps } from 'next';
 import { ParsedUrlQuery } from 'querystring';
-import React from 'react';
+import React, { useState } from 'react';
+import FilterBar from '@/Components/FilterBar';
 
 function SingleCategoryPage({ data }: any) {
-  const products: IProductFromBack[] = data.queries[0].state.data.data.products;
-  console.log(products);
+  let products: IProductFromBack[] = data.queries[0].state.data.data.products;
+  const [filterParams, setFilterParams] = useState<
+    {} | { sort: string; brand: string }
+  >({});
+  console.log(filterParams);
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Box sx={{ width: '30%' }}>
-        <RowRadioButtonsGroup />
-      </Box>
-      <Box
-        sx={{
-          width: '70%',
-          display: 'flex',
-          flexWrap: 'wrap',
-          gap: '30px',
-          justifyContent: 'space-between',
-        }}
-      >
-        {products.map((product) => {
-          return <ProductCard key={product._id} productData={product} />;
-        })}
+    <Box>
+      <Typography sx={{ marginBottom: '50px' }}>
+        جینیس شاپ/{products[0].category.name}
+      </Typography>
+      <Box sx={{ display: 'flex', gap: '6px' }}>
+        <Box
+          sx={{
+            width: '30%',
+            // position: 'fixed',
+            borderWidth: '1px',
+            borderStyle: 'solid',
+            borderColor: theme.palette.secondary.main,
+            borderRadius: '6px',
+          }}
+        >
+          <FilterBar setter={setFilterParams} />
+        </Box>
+        <Box
+          sx={{
+            width: '70%',
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: '30px',
+            justifyContent: 'space-between',
+            marginLeft: 'auto',
+          }}
+        >
+          {products.map((product) => {
+            return <ProductCard key={product._id} productData={product} />;
+          })}
+        </Box>
       </Box>
     </Box>
   );
